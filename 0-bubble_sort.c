8a0aee94514c2a717e51b32fb8377a335c928942
Sarahgad <sarahgad91@gmail.com>
2023-09-22 03:11:13 +0300

    c file pubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ *bubble_sort - sort the array by using bubble sort algorithm
+ *@array: the array of  int element
+ *@size: size of array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	int a = 0;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j + 1] <= array[j])
+			{
+				a = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = a;
+				print_array(array, size);
+			}
+		}
+	}
+}
--- 0-main.c
+++ 0-main.c
@@ -1,3 +1,4 @@
+#include"sort.h"
 /**
  * main - Entry point
  *
