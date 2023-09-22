4c642c31b9fc850e75dfcd1ce228c551fb55fa31
Sarahgad <sarahgad91@gmail.com>
2023-09-22 03:45:38 +0300

    Edit bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,19 +7,24 @@
 void bubble_sort(int *array, size_t size)
 {
 	int a = 0;
+	int b;
 	size_t i, j;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		b = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j + 1] <= array[j])
 			{
+				b = 1;
 				a = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = a;
 				print_array(array, size);
 			}
 		}
+		if(b == 0)
+		break;
 	}
 }
