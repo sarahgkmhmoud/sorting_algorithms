7824e0cf385774c390b1b40065f29389307e63c0
ManarElsaid <elsaidmanar@gmail.com>
2023-09-24 15:48:10 -0700

    test file

---
+++ test/2-selection_sort.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+
+/**
+ * selection_sort -  sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * 
+ * @array: pointer to the array
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
