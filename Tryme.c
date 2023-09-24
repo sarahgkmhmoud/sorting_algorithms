71c644ccc273f35183644f6b42698bb0c83b60d6
Sarahgad <sarahgad91@gmail.com>
2023-09-24 22:53:19 +0300

    insertion

--- Tryme.c
+++ Tryme.c
@@ -1,68 +1,57 @@
-#include"sort.h"
-/**
- *
- * 
- * 
- * 
-*/
-void swap(int *x,int *y)
+
+
+#include "sort.h"
+
+void swap(int *x, int *y)
 {
-int temp = 0;
-temp = *x;
-*x = *y;
-*y = temp;
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
-/**
- * 
- * 
- * 
- * 
-*/
-size_t partitions(int *array, size_t low, size_t high, size_t size)
+
+size_t partition(int *array, size_t low, size_t high, size_t size)
 {
-int pivot;
-size_t i, j;
-pivot = array[high];
-i = low - 1;
-for(j = low; j <= high - 1; j++)
-{
-    if (array[j] < pivot)
+    int pivot = array[high];
+    size_t j;
+    size_t i = low - 1;
+
+    for (j = low; j < high; j++)
     {
-    i++;
-    swap(&array[i], &array[j]);
-    if (array[i] != array[j])
-    print_array(array, size);
+        if (array[j] <= pivot)
+        {
+            i++;
+            if (i != j)
+            {
+                swap(&array[i], &array[j]);
+                print_array(array, size);
+            }
+        }
     }
-}
-    swap(&array[i], &array[high]);
-    if (array[i] != array[high])
-    print_array(array, size);
-        return i+1;
+
+    swap(&array[i + 1], &array[high]);
+                print_array(array, size);
+
+    return i + 1;
 }
 
-/**
- * 
- * 
- * 
- * 
-*/
-void quic_rec(int *array, size_t low, size_t high, size_t size)
+void quicksort_recursive(int *array, ssize_t low, ssize_t high, size_t size)
 {
-
     if (low < high)
     {
-      size_t pivot = partitions(array, low, high, size);
-      quic_rec(array, low, pivot - 1, size);
-      quic_rec(array, pivot + 1, high, size);  
+        size_t pivot = partition(array, low, high, size);
+
+        quicksort_recursive(array, low, pivot - 1, size);
+        quicksort_recursive(array, pivot + 1, high, size);
     }
-    return;
 }
-/**
- *
- * 
- *
-*/
+
 void quick_sort(int *array, size_t size)
 {
-quic_rec(array, 0, size - 1, size);
+    if (array == NULL || size < 2)
+        return;
+
+    quicksort_recursive(array, 0, size - 1, size);
 }
+
+
+
