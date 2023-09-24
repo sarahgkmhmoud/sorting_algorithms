2fb248234df0825578ba3aa49ade166a57cd549f
ManarElsaid <elsaidmanar@gmail.com>
2023-09-24 16:07:13 -0700

    selection sort

--- test/2-selection_sort.c
+++ test/2-selection_sort.c
@@ -8,3 +8,25 @@
  * @size: size of the array
  */
 void selection_sort(int *array, size_t size)
+{
+	size_t minindx, i, j, temp;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		minindx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minindx])
+			{
+				minindx = j;
+			}
+		}
+		temp = array[minindx];
+		array[minindx] = array[j];
+		array[j] = temp;
+		print_array(array, size);
+	}
+}
