d03a5faec4cb62660e9b127a45017b74a6a0860f
ManarElsaid <elsaidmanar@gmail.com>
2023-09-25 06:53:29 -0700

    quick_sort_hoare

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,80 @@
+#include"sort.h"
+/**
+ *swap - swap the integers
+ *@x: integer
+ *@y: integer
+*/
+void swap(int *x, int *y)
+{
+int temp = 0;
+temp = *x;
+*x = *y;
+*y = temp;
+}
+
+/**
+ *partitions_hoare - get the value
+ *@array: array
+ *@low: size_t low
+ *@high: size_t high
+ *@size: size_t size
+ *Return: j on success
+*/
+size_t partitions_hoare(int *array, size_t low, size_t high, size_t size)
+{
+int pivot;
+size_t i, j;
+
+pivot = array[high];
+i = low - 1;
+j = high + 1;
+while (1)
+{
+	do
+	{
+		i++;
+	}while (array[i] < pivot);
+	do
+	{
+		j--;
+	}while (array[j] > pivot);
+	if (i >= j)
+		return (j);
+
+	swap(&array[i], &array[j]);
+	print_array(array, size);
+
+}
+
+}
+
+/**
+ *quic_rec_hoare - recursion
+ *@array: array
+ *@low: ssize_t
+ *@high: ssize_t
+ *@size: size_t
+*/
+void quic_rec_hoare(int *array, ssize_t low, ssize_t high, size_t size)
+{
+
+	if (low < high)
+	{
+	size_t pivot = partitions_hoare(array, low, high, size);
+
+	quic_rec_hoare(array, low, pivot, size);
+	quic_rec_hoare(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ *quick_sort - sorting
+ *@array: array
+ *@size: size
+*/
+void quick_sort_hoare(int *array, size_t size)
+{
+		if (array == NULL || size < 2)
+		return;
+quic_rec_hoare(array, 0, size - 1, size);
+}
--- sort.h
+++ sort.h
@@ -34,6 +34,8 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void quic_rec_hoare(int *array, ssize_t low, ssize_t high, size_t size);
+size_t partitions_hoare(int *array, size_t low, size_t high, size_t size);
 void quicksort_recursive(int *array, ssize_t low, ssize_t high, size_t size);
 size_t partition(int *array, size_t low, size_t high, size_t size);
 #endif /*sort.h*/
